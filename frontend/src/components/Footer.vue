174b4eb46a81af4f2d8bbdf25e37139ada40978b
Dahye021 <heny993472@gmail.com>
2025-11-26 21:54:59 +0900

    25/11/26

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+  <footer class="py-5">
+    <div class="container"> <!-- ① -->
+      <div>&copy; 2025. KDT-SSG9th. KDH Gallery</div> <!-- ② -->
+    </div>
+  </footer>
+</template>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,16 +7,38 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: ()=> import('../views/Home.vue')
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+      path: '/join',
+      name: 'join',
+      component: () => import('../views/Join.vue')
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('../views/Login.vue')
+    },
+    {
+      path: '/cart',
+      name: 'cart',
+      component: () => import('../views/Cart.vue')
+    },
+    {
+      path: '/order',
+      name: 'orderForm',
+      component: () => import('../views/OrderForm.vue')
+    },
+    {
+      path: '/orders',
+      name: 'orders',
+      component: () => import('../views/Orders.vue')
+    },
+    {
+      path: '/orders/:id',
+      name: 'orderDetail',
+      component: () => import('../views/OrderDetail.vue')
+    },
   ]
 })
 
